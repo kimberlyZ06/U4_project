2c888927192b2b0cfcac50262bb2b59ae82648f4
U1_Projecyt <kimberlyz26@nycstudents.net>
2026-02-04 23:33:05 -0500

    Yay!

--- src/Main.java
+++ src/Main.java
@@ -29,17 +29,21 @@ public class Main {
         int highCard = 0;
 
         for (String line : lines) {
-            String[] numbers = line.split(",");
+            int[] counter = new int[13];
+            String[] bid = line.split("\\|");
+            String bidValue = bid[0];
+
+            String[] numbers = bidValue.split(",");
             int[] values = new int[numbers.length];
 
             for (int i = 0; i < numbers.length; i++) {
-                if (numbers[i].equals("Jack")){
+                if (numbers[i].equals("Jack")) {
                     values[i] = 11;
                 } else if (numbers[i].equals("Queen")) {
                     values[i] = 12;
                 } else if (numbers[i].equals("King")) {
                     values[i] = 13;
-                } else if (numbers[i].equals("Ace")){
+                } else if (numbers[i].equals("Ace")) {
                     values[i] = 14;
                 } else {
                     values[i] = Integer.parseInt(numbers[i]);
@@ -47,37 +51,53 @@ public class Main {
             }
             System.out.println(Arrays.toString(values));
 
+            for (int i = 2; i <= 14 ; i++) {
+                for (int j = 0; j < 5; j++){
+                    if (values[j] == i){
+                        counter[i-2] += 1;
+                    }
+                }
+            }
+            System.out.println(Arrays.toString(counter));
+
             int triplet = 0;
             int two = 0;
-            int counter = 0;
+            int singles = 0;
 
-            int index0 = values[0];
-            for (int j = 0; j < values.length; j++) {
-                int firstNum = values[j];
-                values[j] = 0;
-                System.out.println(firstNum);
-                for (int i = j + 1; i < values.length; i++) {
-                    if (firstNum == values[i] && values[i] != 0) {
-                        counter += 1;
-                        values[i] = 0;
-                    }
-
-                    System.out.println("counter: " + counter);
+            for (int i = 0; i < 13; i++) {
+                if (counter[i] == 5){
+                    fiveOfAKind += 1;
+                } else if (counter[i] == 4){
+                    fourOfAKind += 1;
+                } else if (counter[i] == 3){
+                    triplet += 1;
+                } else if (counter[i] == 2) {
+                    two += 1;
+                } else if (counter[i] == 1) {
+                    singles += 1;
                 }
             }
 
-            if (counter == 2){
-                triplet += 1;
-            } else if (counter == 1) {
-                two += 1;
-            }
-
             System.out.println("triplet: " + triplet);
-            System.out.println("two: " + two);
+            System.out.println("double: " + two);
+            System.out.println("singles: " + singles);
+
+            if (triplet == 1 && two == 1){
+                fullHouse += 1;
+            } else if (triplet == 1) {
+                threeOfAKind += 1;
+            } else if (two == 2){
+                twoPair += 1;
+            } else if (two == 1) {
+                onePair += 1;
+            } else if (singles == 5) {
+                highCard += 1;
+            }
         }
+
         System.out.println("five: " + fiveOfAKind);
-        System.out.println("four: " + fourOfAKind);
         System.out.println("full house: " + fullHouse);
+        System.out.println("four: " + fourOfAKind);
         System.out.println("three: " + threeOfAKind);
         System.out.println("two pair: " + twoPair);
         System.out.println("one pair: " + onePair);
